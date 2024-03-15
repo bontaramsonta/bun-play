d2b31aa1b1b30c625f71b1173d247030ff973211
souravdhar <bontaramsonta@gmail.com>
2024-03-15 19:27:50 +0530

    sharp on bun did not work

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
-import { faker } from "@faker-js/faker";
-let n = 0;
-async function gen_logs() {
-  await new Promise((r) => setTimeout(r, 1000));
-  const id = faker.string.uuid();
-  const personName = faker.person.fullName();
-  const timestamp = Date.now();
-  console.log(JSON.stringify({ id, timestamp, personName }));
-  n++;
-  if (n < 100) {
-    gen_logs();
-  }
-}
-gen_logs();
+import { readableStreamToArrayBuffer, serve, sha } from "bun";
+import sharp from "sharp";
+
+serve({
+  async fetch(request, server) {
+    if (request.body !== null) {
+      const body = await readableStreamToArrayBuffer(request.body);
+      const buffer = await sharp(body).tint({ r: 0, g: 0, b: 255 }).toBuffer({
+        resolveWithObject: true,
+        fileOut: "img.png",
+      });
+      console.log(buffer.info);
+      return new Response(buffer.data, {
+        headers: {
+          "Content-Type": "image/png",
+        },
+      });
+    }
+    return new Response("Hello, world!");
+  },
+  port: 3040,
+  development: true,
+});
+console.log("Server started at http://localhost:3040");
